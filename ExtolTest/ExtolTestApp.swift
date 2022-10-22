009d3ebad7389c2679df9b39288dbdb5b082a6ff
Muhammad Zeeshan <zee.1mughal@gmail.com>
2022-10-22 14:39:24 +0500

    Initial Commit

---
+++ ExtolTest/ExtolTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExtolTestApp.swift
+//  ExtolTest
+//
+//  Created by Muhammad Zeeshan on 22/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExtolTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
